843a9ee2db7794520d18f6c182a851ce9f35e31d
CN2015 <164381491+CN2015@users.noreply.github.com>
2025-03-29 09:50:16 +0800

    Delete Shell directory

--- Shell/diy3-360T7-hanwckf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# Copyright (c) 2023 @weigefenxiang
-#
-
-./REVISECONFIG.sh tc-tiny kmod-sched kmod-sched-cake kmod-sched-connmark kmod-sched-ipset
-echo '-----------------------------------------------'
